f4063ef4a0640ea97b203cd43999a56f8d7178a0
sdziura <sabek12@gmail.com>
2026-01-15 15:38:33 +0100

    Add new HPO classicalk baseline experiment

--- src/aqml4msc/experiments/baseline_hpo.py
+++ src/aqml4msc/experiments/baseline_hpo.py
@@ -1,6 +1,7 @@
 from statistics import mean
 
 import optuna
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from torch import nn
 
 from aqml4msc.data.loading import choose_digits, load_data
@@ -75,4 +76,69 @@ def hpo_baseline_1():
 
     study = optuna.create_study(direction="maximize")
     study.optimize(objective, n_trials=20)
+    print(study.best_params)
+
+
+def hpo_baseline_2():
+    def objective(trial):
+        model_params = {
+            "lr": trial.suggest_float("lr", 1e-3, 1e-2),
+            "loss_fn": nn.CrossEntropyLoss(),
+            "num_classes": 3,
+            "input_dim": 14,
+            "hidden_dim_part": [trial.suggest_int("hidden_dim_part", 64, 256)],
+            "output_dim_part": trial.suggest_int("output_dim_part", 64, 256),
+            "hidden_dim_class": [trial.suggest_int("hidden_dim_class", 64, 256)],
+        }
+
+        trainer_params = {
+            "max_epochs": 50,
+            "enable_checkpointing": False,
+            "enable_progress_bar": True,
+            "num_sanity_val_steps": 0,
+            "callbacks": [
+                EpochMetricsTracker(),
+                EarlyStopping(monitor="val_loss", mode="min"),
+            ],
+            "logger": False,
+            "accelerator": "auto",
+            "devices": "auto",
+        }
+        data_params = {
+            "batch_size": trial.suggest_int("batch_size", 32, 128),
+            "num_workers": 8,
+            "digits": [5, 6, 7],
+        }
+
+        experiment_params = {
+            "seed": 42,
+            "n_folds": 5,
+            "parent_run_name": "HPO_classical_MLP_2",
+            "model_name": "Classical_MLP_baseline_2",
+        }
+
+        training = MLPTraining(
+            model_cls=CMLP_1,
+            model_kwargs=model_params,
+            trainer_kwargs=trainer_params,
+            batch_size=data_params["batch_size"],
+        )
+
+        X, y = load_data()
+        X, y = choose_digits(X, y, data_params["digits"])
+        pipeline = ClassificationPipeline()
+        metrics = pipeline.process_data(
+            X=X,
+            y=y,
+            classifier=training,
+            experiment_params=experiment_params,
+            data_params=data_params,
+            model_params=model_params,
+            trainer_params=trainer_params,
+        )
+
+        return mean(metrics["accuracy"])
+
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=1000)
     print(study.best_params)
