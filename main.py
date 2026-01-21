94c1f1f42ddd19aebf85a98fea863fbdba934a58
sdziura <sabek12@gmail.com>
2026-01-21 14:28:55 +0100

    Add quantim hpo experiments

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from aqml4msc.experiments.baseline_hpo import hpo_baseline_1  # noqa: F401
-from aqml4msc.experiments.manual_exp import manual_exp_1  # noqa: F401
+from aqml4msc.experiments.baseline_hpo import (  # noqa: F401
+    hpo_baseline_1,
+    hpo_baseline_2,
+    hpo_baseline_3,
+)
+from aqml4msc.experiments.manual_exp import manual_exp_1, manual_exp_2  # noqa: F401
+from aqml4msc.experiments.quantum_hpo import hpo_quantum_1  # noqa: F401
 
 
 def main():
     print("Hello from aqml4msc!")
-    manual_exp_1()
+    hpo_quantum_1()
 
 
 if __name__ == "__main__":
