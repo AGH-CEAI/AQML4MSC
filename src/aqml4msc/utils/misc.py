effe429226a2fff8d61552548ae9ba65b396b80c
sdziura <sabek12@gmail.com>
2026-02-13 12:43:24 +0100

    Add probe_inputs_and_weight_shapes function

--- src/aqml4msc/utils/misc.py
+++ src/aqml4msc/utils/misc.py
@@ -1,8 +1,11 @@
 import random
+from math import prod
 from typing import Iterator, Tuple
 
 import numpy as np
+import pennylane as qml
 import torch
+from numpy.typing import NDArray
 from sklearn.calibration import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 from torch import from_numpy
@@ -81,3 +84,63 @@ def set_seeds(seed: int):
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
+
+
+def probe_inputs_and_weight_shapes(
+    circuit: qml.QNode,
+) -> dict[str, tuple[int, ...]]:
+    """Brute-force, very naive and with lots of assumptions. But that's how :mod:`pennylane` wants to play.
+
+    We assume the following:
+    1) there are only 2 inputs to the QNode: inputs (however named) and weights (however named)
+    2) they can be given flat
+    3) the circuit uses all the input values before using any of the weights (embedding on all wires does the trick)
+    4) the bad-input ValueError happen before bad-weights IndexError
+
+    :return: _description_
+    :rtype: _type_
+    """
+    shapes = {}
+    # input_shape = PennylaneToQasm3._probe_input_shape(circuit)
+    try:
+        qml.specs(circuit)(np.zeros(10000), [[]])  # Must be high.
+    except ValueError as e:
+        if "Input state must be of length " in str(e):
+            # e reads:
+            #
+            # Input state must be of length X or smaller to be padded; got length Y.
+            shapes["inputs"] = (int(str(e).split(" ")[6]),)
+        if "Features must be of length" in str(e):
+            # e reads:
+            #
+            # Features must be of length X or less; got length Y.
+            shapes["inputs"] = (int(str(e).split(" ")[5]),)
+
+    n_weights: int = 0
+    weights: NDArray[int] = np.zeros(n_weights)  # type: ignore
+    while True:
+        try:
+            # print(f"Trying: {weights}")
+            qml.specs(circuit)(np.zeros(shapes["inputs"]), weights)
+            shapes["weights"] = (n_weights,)
+            # print(shapes)
+            return shapes
+        except ValueError as e:
+            if "into shape" in str(e):  # This solves `aqmlator`-models problems.
+                n_weights += prod(
+                    [int(s) for s in str(e).split(" ")[-1][1:-1].split(",")]
+                )
+                weights = np.zeros(n_weights)
+                continue
+            if "must have last dimension of" in str(e):
+                # e reads:
+                #
+                # Weights tensor must have last dimension of length X; got Y
+                #
+                # We extract X again.
+                # We assume 1 layer per layer call.
+                shapes["weights"] = (1, int(str(e).split(" ")[8][:-1]))
+                return shapes
+            if "must be 2-dimensional or 3-dimensional" in str(e):
+                # Assume 1 layer per layer call.
+                weights = np.zeros((1, n_weights))
