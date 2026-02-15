c164d97b0f06213bd35293ba906546686399baa7
Tomasz Rybotycki <rybotycki.tomasz+git@gmail.com>
2026-02-15 04:17:50 +0100

    update(main): Update main for aqml experiment. Confirm it's runnable.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from aqml4msc.experiments.ansatz_search import main as aqml_hpo
+
+"""
 from aqml4msc.experiments.baseline_hpo import (  # noqa: F401
     hpo_baseline_1,
     hpo_baseline_2,
@@ -5,11 +8,12 @@ from aqml4msc.experiments.baseline_hpo import (  # noqa: F401
 )
 from aqml4msc.experiments.manual_exp import manual_exp_1, manual_exp_2  # noqa: F401
 from aqml4msc.experiments.quantum_hpo import hpo_quantum_1  # noqa: F401
+"""
 
 
 def main():
     print("Hello from aqml4msc!")
-    hpo_quantum_1()
+    aqml_hpo()
 
 
 if __name__ == "__main__":
