aea6c8615232236fc6d93389393a2d075ec126c5
sdziura <sabek12@gmail.com>
2026-04-24 16:17:14 +0200

    feat(data): add splits, encode and decode functions to Dataset classes

--- aqml4msc/datasets/base_dataset.py
+++ aqml4msc/datasets/base_dataset.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
 
+from sklearn.preprocessing import LabelEncoder
+
 
 class BaseDataset(ABC):
     def __init__(self, config: dict):
         self.config = config
+        self.label_encoder = LabelEncoder()
 
     @abstractmethod
     def load_raw(self):
@@ -19,4 +22,8 @@ class BaseDataset(ABC):
 
     @abstractmethod
     def set_splits(self, train_idx, test_idx):
+        raise NotImplementedError
+
+    @abstractmethod
+    def decode_labels(self, y: list[int]) -> list[int]:
         raise NotImplementedError
--- aqml4msc/datasets/mnist.py
+++ aqml4msc/datasets/mnist.py
@@ -27,9 +27,17 @@ class MnistDataset(BaseDataset):
     def clean_data(self):
         indexes = np.isin(self.y_raw, self.config["digits"])
         self.x_clean, self.y_clean = self.x_raw[indexes], self.y_raw[indexes]
+        self.label_encoder.fit(self.y_clean)
+        self.y_clean = self.label_encoder.transform(self.y_clean)  # type: ignore
 
     def preprocess(self):
         self.x_top, self.x_bottom = preprocess_pipeline(self.x_clean)
 
     def set_splits(self, train_idx, test_idx):
-        raise NotImplementedError
+        self.train_data = (self.x_top[train_idx], self.x_bottom[train_idx])
+        self.train_labels = self.y_clean[train_idx]
+        self.val_data = (self.x_top[test_idx], self.x_bottom[test_idx])
+        self.val_labels = self.y_clean[test_idx]
+
+    def decode_labels(self, y: list[int]) -> list[int]:
+        return self.label_encoder.inverse_transform(y)  # type: ignore
