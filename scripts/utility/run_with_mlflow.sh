1bdb1de1165d5d11ba6b2af43e15805c37fc0bee
sdziura <sabek12@gmail.com>
2026-05-21 16:42:48 +0200

    feat(docker): run multiple scripts in docker

--- scripts/utility/run_with_mlflow.sh
+++ scripts/utility/run_with_mlflow.sh
@@ -3,22 +3,14 @@ set -euo pipefail
 
 export UV_ENV_FILE=/app/aqml4msc.env
 
-echo "Starting MLflow server..."
-uv run mlflow server \
-    --backend-store-uri "${MLFLOW_TRACKING_URI}"\
-    --host 0.0.0.0 \
-    --port 5000 \
-    --workers 1 \
-    --default-artifact-root "${MLFLOW_ARTIFACTS_ROOT}" &
-MLFLOW_PID=$!
-echo !!
-
-# Ensure MLflow is killed on script exit, error, or Ctrl+C
-trap "echo 'Stopping all MLflow processes...'; kill $MLFLOW_PID 2>/dev/null; pkill -f 'uvicorn.*mlflow.server.fastapi_app' 2>/dev/null; wait" EXIT
-
-sleep 5
-
-uv run main.py
+if [ $# -eq 0 ]; then
+    uv run main.py
+else
+    for script in "$@"; do
+        echo "Running $script..."
+        uv run "$script"
+    done
+fi
 
 # Explicit exit
 exit 0
