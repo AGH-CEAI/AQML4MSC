ef827467c179f52782d2850bd2eefc406daabeaa
sdziura <sabek12@gmail.com>
2026-05-21 16:59:29 +0200

    feat(exp): add experiments with smaller multimodal model

--- scripts/experiments/seeds_modality_comparison/classsical_manual_chkpts.py
+++ scripts/experiments/seeds_modality_comparison/classsical_manual_chkpts.py
@@ -252,6 +252,88 @@ def manual_classical_multimodal_1():
     return mean(metrics["accuracy"])
 
 
+def manual_classical_multimodal_2():
+
+    model_params = {
+        "lr": 1e-3,
+        "loss_fn": nn.CrossEntropyLoss(),
+        "num_classes": 3,
+        "input_dim": 12,
+        "hidden_dim_part": [0],
+        "output_dim_part": [],
+        "hidden_dim_class": [100],
+    }
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="checkpoints/seeds",
+        monitor="val_acc",
+        mode="max",
+        save_top_k=1,
+    )
+
+    trainer_params = {
+        "max_epochs": 300,
+        "enable_checkpointing": True,
+        "enable_progress_bar": True,
+        "num_sanity_val_steps": 0,
+        "accelerator": "auto",
+        "devices": "auto",
+        "callbacks": [checkpoint_callback],
+    }
+    data_params = {
+        "batch_size": 32,
+        "num_workers": 8,
+        "pca_img_components": 6,
+        "pca_tab_components": 6,
+    }
+
+    experiment_params = {
+        "seed": 42,
+        "n_folds": 5,
+        "parent_run_name": "Classical_MLP_Multimodal_6_6",
+        "model_name": "Classical_MLP_baseline",
+    }
+
+    # Initialize the dataset with the specified data parameters
+    dataset = SeedsDataset(config=data_params)
+
+    extractor_factories = [
+        nn.Identity,
+        nn.Identity,
+    ]
+
+    fusion_factory = partial(
+        ConcatMLPFusion,
+        model_params["input_dim"],
+        model_params["hidden_dim_class"],
+        model_params["num_classes"],
+    )
+    main_model_factory = partial(
+        BaseMLPModel,
+        model_params=model_params,
+        extractor_factories=extractor_factories,
+        fusion_factory=fusion_factory,
+    )
+
+    training = MLPTraining(trainer_kwargs=trainer_params)
+
+    # Initialize the classification pipeline: ClassificationPipeline
+    pipeline = ClassificationPipeline()
+    metrics = pipeline.process_data(
+        model_factory=main_model_factory,
+        dataset=dataset,
+        training=training,
+        params={
+            "experiment_params": experiment_params,
+            "data_params": data_params,
+            "model_params": model_params,
+            "trainer_params": trainer_params,
+        },
+    )
+
+    return mean(metrics["accuracy"])
+
+
 def main() -> None:
     """Calls the experiment."""
     setup_mlflow(experiment_name="Seeds_Multimodal_Classification")
@@ -260,6 +342,8 @@ def main() -> None:
     metrics = manual_classical_images_1()
     print(metrics)
     metrics = manual_classical_multimodal_1()
+    print(metrics)
+    metrics = manual_classical_multimodal_2()
     print(metrics)
 
 
