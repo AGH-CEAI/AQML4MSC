cbe7d48d13593ad8361778bc724557b49aacbb30
sdziura <sabek12@gmail.com>
2026-06-24 09:05:09 +0200

    feat(exp): extend statistical comparison script

--- scripts/experiments/seeds_modality_comparison/comparison.py
+++ scripts/experiments/seeds_modality_comparison/comparison.py
@@ -16,6 +16,12 @@ results = {
     "qnn_strongly_images": [0.759, 0.707, 0.759, 0.737, 0.754],
     "qnn_strongly_multimodal": [0.810, 0.724, 0.741, 0.684, 0.754],
     "qnn_strongly_multimodal_small": [0.724, 0.690, 0.759, 0.737, 0.737],
+    "qnn_strongly_probs_tab": [0.828, 0.741, 0.845, 0.316, 0.754],
+    "qnn_strongly_probs_images": [0.707, 0.724, 0.741, 0.737, 0.719],
+    "qnn_strongly_probs_multimodal": [0.741, 0.776, 0.776, 0.754, 0.754],
+    "qnn_strongly_probs_multimodal_small": [0.810, 0.845, 0.845, 0.825, 0.772],
+    "qnn_strongly_probs_multimodal_bilinear": [0.776, 0.914, 0.862, 0.895, 0.860],
+    "qnn_strongly_probs_multimodal_bilinear_small": [0.828, 0.931, 0.897, 0.877, 0.877],
 }
 
 fight_list = [
@@ -31,6 +37,14 @@ fight_list = [
     ["qnn_strongly_multimodal", "qnn_strongly_images"],
     ["qnn_strongly_multimodal_small", "qnn_strongly_tab"],
     ["qnn_strongly_multimodal_small", "qnn_strongly_images"],
+    ["qnn_strongly_probs_multimodal", "qnn_strongly_probs_tab"],
+    ["qnn_strongly_probs_multimodal", "qnn_strongly_probs_images"],
+    ["qnn_strongly_probs_multimodal_small", "qnn_strongly_probs_tab"],
+    ["qnn_strongly_probs_multimodal_small", "qnn_strongly_probs_images"],
+    ["qnn_strongly_probs_multimodal_bilinear", "qnn_strongly_probs_tab"],
+    ["qnn_strongly_probs_multimodal_bilinear", "qnn_strongly_probs_images"],
+    ["qnn_strongly_probs_multimodal_bilinear_small", "qnn_strongly_probs_tab"],
+    ["qnn_strongly_probs_multimodal_bilinear_small", "qnn_strongly_probs_images"],
 ]
 
 for res1, res2 in fight_list:
