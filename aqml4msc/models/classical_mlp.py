457e6774e3bec767627097fe768e56065c168343
sdziura <sabek12@gmail.com>
2026-07-24 15:13:57 +0200

    feat(models): add classical_1l_sigmoid_mlp model

--- aqml4msc/models/classical_mlp.py
+++ aqml4msc/models/classical_mlp.py
@@ -2,11 +2,20 @@ import torch
 import torch.nn as nn
 
 
-def classical_2l_mlp(input_dim, hidden_dim, output_dim):
+def classical_2l_mlp(
+    input_dim: int, hidden_dim: list[int], output_dim: int
+) -> nn.Module:
     return nn.Sequential(
         nn.Linear(input_dim, hidden_dim[0]),
         nn.ReLU(),
         nn.Linear(hidden_dim[0], output_dim),
+    )
+
+
+def classical_1l_sigmoid_mlp(input_dim: int, output_dim: int) -> nn.Module:
+    return nn.Sequential(
+        nn.Linear(input_dim, output_dim),
+        nn.Sigmoid(),
     )
 
 
